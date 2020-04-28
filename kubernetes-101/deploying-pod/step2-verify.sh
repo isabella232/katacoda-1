fa42976afc6d14b58906b2520e454fe8a91f4997
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-04-28 15:50:29 +0100

    fix

--- kubernetes-101/deploying-pod/step2-verify.sh
+++ kubernetes-101/deploying-pod/step2-verify.sh
@@ -1,3 +1,3 @@
-var1=$(kubectl get pods -o jsonpath="{.items[?(@.metadata.name)].metadata.name}" | grep -o http )
+var1=$(kubectl get pods -o jsonpath="{.items[?(@.metadata.name)].metadata.name}" | grep -ow http )
 var2="http"
 [ "$var1" == $var2 ] && echo "done"
