595c537ec76cc0ce4d52b25cfe4156ff04fb4deb
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-05-03 23:18:31 +0100

    merge with master

---
+++ kubernetes-101/loadbalancing/step3-verify.sh
@@ -0,0 +1,2 @@
+var1=$(kubectl get pods -n kubesystem -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.containers[].image}{"\n"}{end}'| grep -ow "http katacoda/docker-http-server:latest")
+var2="http katacoda/docker-http-server:latest"
--- kubernetes-101/services/step1-externalip-verify.sh
+++ kubernetes-101/services/step1-externalip-verify.sh
@@ -1,7 +1,7 @@
 # Verify the http pod
 var1=$(kubectl get svc -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.type}{" "}{@.spec.ports[].port}{" "}{@.spec.externalIPs}{"\n"}{end}'| grep -ow "externalhttp ClusterIP 8002 \[$HOST_IP\]")
-var2="externalhttp ClusterIP 80 [$HOST_IP]"
+var2="externalhttp ClusterIP 8002 [$HOST_IP]"
 var3=$(kubectl get svc -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.type}{" "}{@.spec.ports[].port}{" "}{@.spec.externalIPs}{"\n"}{end}'| grep -ow "externalyaml ClusterIP 8003 \[$HOST_IP\]")
-var4="externalyaml ClusterIP 80 [$HOST_IP]"
+var4="externalyaml ClusterIP 8003 [$HOST_IP]"
 
 [ "$var1" == "$var2" ] && [ "$var3" == "$var4" ] &&echo "done"
