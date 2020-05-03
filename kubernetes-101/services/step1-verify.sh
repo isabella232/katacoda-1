1ce96c32615eefb9367af4a9196d41db41a98570
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-05-03 22:26:50 +0100

    merge with master

--- kubernetes-101/services/step1-verify.sh
+++ kubernetes-101/services/step1-verify.sh
@@ -1,5 +1,5 @@
 # Verify the http pod
 var1=$(kubectl get pods -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.containers[].image}{"\n"}{end}'| grep -ow "http katacoda/docker-http-server:latest")
-var2="http katacoda/docker-http-server:latest"
+var2="http"
 
 [ "$var1" == "$var2" ] && echo "done"
