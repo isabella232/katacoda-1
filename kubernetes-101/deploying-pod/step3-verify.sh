fe43b592b10d48cc154da1f40971d11d49e030ad
Oli Keys <oli.keys@condenast.com>
2020-05-04 15:25:24 +0100

    v1 instead of latest

--- kubernetes-101/deploying-pod/step3-verify.sh
+++ kubernetes-101/deploying-pod/step3-verify.sh
@@ -1,5 +1,5 @@
 # Verify the http-yaml pod
-var1=$(kubectl get pods -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.containers[].image}{"\n"}{end}'| grep -ow "http-yaml katacoda/docker-http-server:latest")
-var2="http-yaml katacoda/docker-http-server:latest"
+var1=$(kubectl get pods -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.containers[].image}{"\n"}{end}'| grep -ow "http-yaml katacoda/docker-http-server:v1")
+var2="http-yaml katacoda/docker-http-server:v1"
 
 [ "$var1" == "$var2" ] && echo "done"
