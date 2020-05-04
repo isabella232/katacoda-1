63efb5c676a8e7103d1dc4b3d42b4b6c8e5a02f4
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-05-04 08:45:11 +0100

    merge with master

--- kubernetes-101/services/step1-externalip-verify.sh
+++ kubernetes-101/services/step1-externalip-verify.sh
@@ -4,5 +4,10 @@ export externalvar1=$(kubectl get svc -o jsonpath='{range .items[*]}{@.metadata.
 export externalvar2="externalhttp ClusterIP 8002 [$HOST_IP]"
 export externalvar3=$(kubectl get svc -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.type}{" "}{@.spec.ports[].port}{" "}{@.spec.externalIPs}{"\n"}{end}'| grep -ow "externalyaml ClusterIP 8003 \[$HOST_IP\]")
 export externalvar4="externalyaml ClusterIP 8003 [$HOST_IP]"
+echo "$externalvar1">a.txt
+echo "$externalvar2">b.txt
+
+echo "$externalvar3">c.txt
+echo "$externalvar4">d.txt
 
 [ "$externalvar1" == "$externalvar2" ] && [ "$externalvar3" == "$externalvar4" ] &&echo "done"
