1850a0a6fae15f290f2704953747799327b0638d
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-05-04 08:36:05 +0100

    merge with master

--- kubernetes-101/services/step1-externalip-verify.sh
+++ kubernetes-101/services/step1-externalip-verify.sh
@@ -1,4 +1,5 @@
 # Verify the http pod
+echo "external"
 export externalvar1=$(kubectl get svc -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.type}{" "}{@.spec.ports[].port}{" "}{@.spec.externalIPs}{"\n"}{end}'| grep -ow "externalhttp ClusterIP 8002 \[$HOST_IP\]")
 export externalvar2="externalhttp ClusterIP 8002 [$HOST_IP]"
 export externalvar3=$(kubectl get svc -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.type}{" "}{@.spec.ports[].port}{" "}{@.spec.externalIPs}{"\n"}{end}'| grep -ow "externalyaml ClusterIP 8003 \[$HOST_IP\]")
