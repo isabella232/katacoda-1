b0bfe02c27b3bbf883565a38875344f8dc277c01
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-05-04 08:50:30 +0100

    merge with master

--- kubernetes-101/services/foreground.sh
+++ kubernetes-101/services/foreground.sh
@@ -1,3 +1,4 @@
 minikube start --extra-config=apiserver.cloud-provider=aws --extra-config=controller-manager.cloud-provider=aws --wait=false
 mkdir -p kubernetes101; cd /kubernetes101/
 export HOST_IP="[[HOST_IP]]"
+echo $HOST_IP > hostip
--- kubernetes-101/services/step1-externalip-verify.sh
+++ kubernetes-101/services/step1-externalip-verify.sh
@@ -1,13 +1,9 @@
 # Verify the http pod
-echo "external"
+
+export HOST_IP= cat hostip
 export externalvar1=$(kubectl get svc -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.type}{" "}{@.spec.ports[].port}{" "}{@.spec.externalIPs}{"\n"}{end}'| grep -ow "externalhttp ClusterIP 8002 \[$HOST_IP\]")
 export externalvar2="externalhttp ClusterIP 8002 [$HOST_IP]"
 export externalvar3=$(kubectl get svc -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.type}{" "}{@.spec.ports[].port}{" "}{@.spec.externalIPs}{"\n"}{end}'| grep -ow "externalyaml ClusterIP 8003 \[$HOST_IP\]")
 export externalvar4="externalyaml ClusterIP 8003 [$HOST_IP]"
-echo "$externalvar1">a.txt
-echo "$externalvar2">b.txt
-
-echo "$externalvar3">c.txt
-echo "$externalvar4">d.txt
 
 [ "$externalvar1" == "$externalvar2" ] && [ "$externalvar3" == "$externalvar4" ] &&echo "done"
